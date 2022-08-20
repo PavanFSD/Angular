933997cd971cc09142f3309c19bf44fc78e43d59
PavanFSD <interview8627@gmail.com>
2022-08-20 17:58:08 +0530

    adding two way binding

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -7,22 +7,5 @@ import { Component } from '@angular/core';
 })
 // AppComponent.ts
 export class AppComponent {
- 
-  x:number=0;
- name:String="";
-  increment(){
-    
-this.x = this.x+1;
-  }
-
-  decrement(){
-    this.x = this.x-1;
-      }
-
-      changeName(evn:any){
-        console.log(evn);
-        console.log(evn.target);
-        console.log(evn.target.value);
-        this.name = evn.target.value;
-      }
+ city:String = "hyd";
 }
