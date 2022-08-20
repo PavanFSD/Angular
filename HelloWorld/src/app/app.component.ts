85f318385ec274d69348efae5e2a47360e262d53
PavanFSD <interview8627@gmail.com>
2022-08-20 16:24:25 +0530

    adding style binding

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -5,11 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// AppComponent.ts
 export class AppComponent {
   title = 'HelloWorld';
-  name:String = "pavan";
-  isdisabled : boolean = false;
-  isActive:boolean=true;
-  isapplyBG:boolean=true;
-  activeClass:String='active';
+  apply_color:String = "red";
+  
 }
