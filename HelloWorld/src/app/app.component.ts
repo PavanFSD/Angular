79fc0b6aa6965d6a24aa2601cf29ed9762bc0003
PavanFSD <interview8627@gmail.com>
2022-08-22 14:59:02 +0530

    adding ng for directive code

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 // AppComponent.ts
 export class AppComponent {
 data = ["hyd","bnglr","chennai"]
+
+ emp_data = [
+  {"name":"Ram", "email":"ram@gmail.com", "age":23},
+  {"name":"Shyam", "email":"shyam23@gmail.com", "age":28},
+  {"name":"John", "email":"john@gmail.com", "age":33},
+  {"name":"Bob", "email":"bob32@gmail.com", "age":41}
+  ]
+  
 }
